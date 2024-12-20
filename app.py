9f24f84826dc0d535f03aba64ace91c270173a24
sushovan1 <sushovanrkm@gmail.com>
2024-12-20 18:47:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,3 +75,6 @@ async def predict_loan_eligibility(request: LoanRequest):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred during prediction: {e}")
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
